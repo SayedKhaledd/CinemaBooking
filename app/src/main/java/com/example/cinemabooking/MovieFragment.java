7ccc84be910842a7d94672f7a3e2c5bcb0413262
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2021-05-16 13:52:46 +0200

    Update: connecting search with movie fragment

--- app/src/main/java/com/example/cinemabooking/MovieFragment.java
+++ app/src/main/java/com/example/cinemabooking/MovieFragment.java
@@ -1,5 +1,6 @@
 package com.example.cinemabooking;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +61,18 @@ public class MovieFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie, container, false);
+       View view =inflater.inflate(R.layout.fragment_movie, container, false);
+        Button button=view.findViewById(R.id.button_movie);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getActivity(),Search.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+        return view;
     }
 }
