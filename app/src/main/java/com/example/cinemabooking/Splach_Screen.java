60905592b6d1a5d01795b4ad4ca7cefec234b83b
MohamedKhaled999 <68979528+MohamedKhaled999@users.noreply.github.com>
2021-05-26 18:10:50 +0200

    splach

---
+++ app/src/main/java/com/example/cinemabooking/Splach_Screen.java
@@ -0,0 +1,32 @@
+package com.example.cinemabooking;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splach_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splach__screen);
+        getSupportActionBar().hide();
+        Thread thread = new Thread() {
+
+            @Override
+            public void run() {
+
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), Login.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+}
