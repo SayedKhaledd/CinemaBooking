81183511c5f82081741ed31240acd93028a6ba11
MohamedKhaled999 <68979528+MohamedKhaled999@users.noreply.github.com>
2021-05-28 20:12:25 +0200

    con..

--- app/src/main/java/com/example/cinemabooking/Login.java
+++ app/src/main/java/com/example/cinemabooking/Login.java
@@ -8,12 +8,13 @@ import android.view.View;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
-TextView skip;
+TextView skip,signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         skip =findViewById(R.id.skip);
+        signup =findViewById(R.id.sign_up);
         skip.setOnClickListener(this);
     }
 
@@ -23,6 +24,10 @@ TextView skip;
             Intent intent= new Intent(this,MainActivity.class);
             startActivity(intent);
 
+        }else if(v.getId()==R.id.sign_up){
+
+            Intent intent= new Intent(this,Signup.class);
+            startActivity(intent);
         }
     }
 }
